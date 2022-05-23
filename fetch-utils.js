d1e04b5c6e8c1312616435ff58b59c56c28f6893
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-23 12:12:08 -0700

    linking supabase / input to supabase

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Create your own supabase database using the provided seeds.sql file
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://xsjxkslcnzexkeuqopsq.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhzanhrc2xjbnpleGtldXFvcHNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIyOTI0MzIsImV4cCI6MTk2Nzg2ODQzMn0.6PW-I5jDnoqhgO6Egpvc5fEpprJ1jHfmAODRlCynp2I';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -9,7 +9,7 @@ export function getUser() {
 }
 
 export async function getFamilies() {
-    const response = await client.from('loving_families').select('*, name(*)');
+    const response = await client.from('fuzzy_bunnies').select('*, family_id(*)');
     // fetch all families and their bunnies
     if (response.error) {
         console.error(response.error.message);
